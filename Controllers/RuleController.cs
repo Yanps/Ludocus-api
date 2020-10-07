30911c21f9434a272d2504276c82d5ce100a6b9b
Brunelli Gabriel Cupello <brunelli.cupello@live.jp>
2020-10-07 11:28:57 -0300

    Implements RuleController

--- Controllers/RuleController.cs
+++ Controllers/RuleController.cs
@@ -2,7 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoWrapper.Wrappers;
+using LudocusApi.Models;
+using LudocusApi.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Nest;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,36 +17,147 @@ namespace LudocusApi.Controllers
     [ApiController]
     public class RuleController : ControllerBase
     {
+        #region Properties
+        IConfiguration _configuration;
+
+        ElasticClient _client;
+
+        int _defaultSize;
+        #endregion
+
+        #region Get all Rules
+        // Gets all Rules
         // GET: api/<RuleController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public ApiResponse Get()
         {
-            return new string[] { "value1", "value2" };
-        }
+            // Verifies if user has authorization
+            // TODO
+            // return new ApiResponse(null, 401);
 
-        // GET api/<RuleController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+            // Queries Rules
+            ISearchResponse<Rule> searchResponse = _client.Search<Rule>(s => s
+                .From(0)
+                .Size(this._defaultSize)
+            );
+
+            if (searchResponse.IsValid == true)
+            {
+                return new ApiResponse(searchResponse.Documents, 200);
+            }
+
+            return new ApiResponse(null, 204);
+        }
+        #endregion
+
+        #region Get Rule by uid
+        // GET api/<RuleController>/81ba942aaca411e9b143023fad48cc44
+        [HttpGet("{rule_uid}")]
+        public ApiResponse Get(string rule_uid)
         {
-            return "value";
-        }
+            // Verifies if user has authorization
+            // TODO
+            // return new ApiResponse(null, 401);
 
+            // Queries Rules by uid
+            IGetResponse<Rule> getResponse = _client.Get<Rule>(rule_uid);
+
+            if (getResponse.IsValid == true)
+            {
+                // If has found Rule, returns 200
+                return new ApiResponse(getResponse.Source, 200);
+            }
+
+            // Returns not found
+            return new ApiResponse(null, 204);
+        }
+        #endregion
+
+        #region Create new Rule
         // POST api/<RuleController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public ApiResponse Post([FromBody] Rule rule)
         {
-        }
+            // Verifies if user has authorization
+            // TODO
+            // return new ApiResponse(null, 401);
 
-        // PUT api/<RuleController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+            // Indexes Rule's document
+            IndexResponse indexResponse = _client.IndexDocument(rule);
 
-        // DELETE api/<RuleController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            if (indexResponse.IsValid == true)
+            {
+                // If has created Rule, returns 201
+                return new ApiResponse(indexResponse.Id, 201);
+            }
+
+            // If hasn't created Rule, returns 500
+            return new ApiResponse("Internal server error when trying to create Rule", null, 500);
         }
+        #endregion
+
+        #region Edit Rule
+        // PUT api/<RuleController>/81ba942aaca411e9b143023fad48cc44
+        [HttpPut("{rule_uid}")]
+        public ApiResponse Put(string rule_uid, [FromBody] Rule rule)
+        {
+            // Verifies if user has authorization
+            // TODO
+            // return new ApiResponse(null, 401);
+
+            // Updates Rule's document
+            UpdateResponse<Rule> response = _client.Update<Rule, Rule>(
+                new DocumentPath<Rule>(rule_uid),
+                u => u
+                    .Index("rules")
+                    .Doc(rule)
+            );
+
+            if (response.IsValid == true)
+            {
+                // If has updated Rule, returns 200
+                return new ApiResponse("Updated successfully", null, 200);
+            }
+
+            // If hasn't updated Rule, returns 500
+            return new ApiResponse("Internal server error when trying to update Rule", null, 500);
+        }
+        #endregion
+
+        #region Delete Rule by uid
+        // DELETE api/<RuleController>/81ba942aaca411e9b143023fad48cc44
+        [HttpDelete("{rule_uid}")]
+        public ApiResponse Delete(string rule_uid)
+        {
+            // Verifies if user has authorization
+            // TODO
+            // return new ApiResponse(null, 401);
+
+            // Deletes Rule's document
+            DeleteResponse response = _client.Delete<Organization>(rule_uid);
+
+            if (response.IsValid == true)
+            {
+                // If has deleted Rule, returns 200
+                return new ApiResponse("Deleted successfully", null, 200);
+            }
+
+            // If hasn't deleted Rule, returns 500
+            return new ApiResponse("Internal server error when trying to delete Rule", null, 500);
+        }
+        #endregion
+
+        #region Constructor
+        public RuleController(IConfiguration configuration)
+        {
+            // Sets configuration
+            this._configuration = configuration;
+
+            // Connects to ES
+            this._client = new ElasticsearchService(this._configuration, "rules").Get();
+
+            this._defaultSize = Int32.Parse(this._configuration.GetSection("ElasticsearchSettings").GetSection("defaultSize").Value);
+        }
+        #endregion
     }
 }
