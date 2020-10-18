baa0b62adfce7c3df038b5e9caa9af0f1de75ba3
Brunelli Gabriel Cupello <brunelli.cupello@live.jp>
2020-10-18 19:13:03 -0300

    Implements Cors

--- Startup.cs
+++ Startup.cs
@@ -27,22 +27,24 @@ namespace LudocusApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddCors(options =>
-            {
-                options.AddPolicy("Policy1",
-                    builder =>
-                    {
-                        builder.WithOrigins("/*");
-                    });
+            services.AddCors(
+                //options =>
+            //{
+            //    options.AddPolicy("Policy1",
+            //        builder =>
+            //        {
+            //            builder.WithOrigins("/*");
+            //        });
 
-                options.AddPolicy("AnotherPolicy",
-                    builder =>
-                    {
-                        builder.WithOrigins("http://www.contoso.com")
-                                            .AllowAnyHeader()
-                                            .AllowAnyMethod();
-                    });
-            });
+            //    options.AddPolicy("AnotherPolicy",
+            //        builder =>
+            //        {
+            //            builder.WithOrigins("http://www.contoso.com")
+            //                                .AllowAnyHeader()
+            //                                .AllowAnyMethod();
+            //        });
+            //}
+            );
 
             services.AddControllers();
 
@@ -63,7 +65,11 @@ namespace LudocusApi
 
             app.UseRouting();
 
-            app.UseCors();
+            app.UseCors(builder => builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+            );
 
             app.UseAuthorization();
 
