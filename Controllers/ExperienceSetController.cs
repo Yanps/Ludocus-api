52e7c7b09e725eedba45e548f033d574252b2bdc
Brunelli Gabriel Cupello <brunelli.cupello@live.jp>
2020-10-23 16:34:25 -0300

    Implements Delete Experiences Sets by Metric Uid method

--- Controllers/ExperienceSetController.cs
+++ Controllers/ExperienceSetController.cs
@@ -290,6 +290,36 @@ namespace LudocusApi.Controllers
         }
         #endregion
 
+        #region Delete Experience Sets by Metric Uid
+        // DELETE api/<ExperienceSetController>/metric/33ba942aaca411e9b143023fad48cc33
+        [HttpDelete("metric/{metric_uid}")]
+        public ApiResponse DeleteByMetricUid(string metric_uid)
+        {
+            // Verifies if user has authorization
+            // TODO
+            // return new ApiResponse(null, 401);
+
+            // Deletes Experience Sets documents by Metric uid
+            DeleteByQueryResponse deleteResponse = _client.DeleteByQuery<ExperienceSet>(q => q
+                .Query(rq => rq
+                    .Match(m => m
+                        .Field(f => f.metric_uid)
+                        .Query(metric_uid)
+                    )
+                )
+            );
+
+            if (deleteResponse.IsValid == true)
+            {
+                // If has deleted Experience Sets, returns 200
+                return new ApiResponse("Deleted successfully", null, 200);
+            }
+
+            // If hasn't deleted Experience Sets, returns 500
+            return new ApiResponse("Internal server error when trying to delete Experience Sets", null, 500);
+        }
+        #endregion
+
         #region Constructor
         public ExperienceSetController(IConfiguration configuration) : base(configuration, "experiencesets")
         {
