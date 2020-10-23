866b131a9791a1b0f344f510b38045b01047e433
Brunelli Gabriel Cupello <brunelli.cupello@live.jp>
2020-10-23 15:01:48 -0300

    Implements Delete by Metric uid for Metrics Values

--- Controllers/MetricValuesController.cs
+++ Controllers/MetricValuesController.cs
@@ -344,6 +344,35 @@ namespace LudocusApi.Controllers
         }
         #endregion
 
+        #region Delete Metrics Values by Metric uid
+        // DELETE api/<MetricValuesController>/user/fd8546c4312d11e985c4641c67730998
+        [HttpDelete("metric/{metric_uid}")]
+        public ApiResponse DeleteByMetricUid(string metric_uid)
+        {
+            // Verifies if user has authorization
+            // TODO
+            // return new ApiResponse(null, 401);
+
+            var deleteResponse = _client.DeleteByQuery<MetricValues>(q => q
+                .Query(rq => rq
+                    .Match(m => m
+                        .Field(f => f.metric_uid)
+                        .Query(metric_uid)
+                    )
+                )
+            );
+
+            if (deleteResponse.IsValid == true)
+            {
+                // If has deleted all Metrics Values, returns 200
+                return new ApiResponse("Deleted successfully", null, 200);
+            }
+
+            // If hasn't deleted Metric Values, returns 500
+            return new ApiResponse("Internal server error when trying to delete Metric Values", null, 500);
+        }
+        #endregion
+
         #region Constructor
         public MetricValuesController(IConfiguration configuration) : base(configuration, "metrics_values")
         {
