079ac807f8880d77d03bd4e4be9d31fb13f89b56
Brunelli Gabriel Cupello <brunelli.cupello@live.jp>
2020-10-24 06:50:51 -0300

    Creates RuleResponse oject for Get All Rules method

--- Models/Rule.cs
+++ Models/Rule.cs
@@ -28,5 +28,31 @@ namespace LudocusApi.Models
         public string rule_value { get; set; }
 
         public DateTime create_date { get; set; }
+
+        public class RuleResponse : Rule
+        {
+            public string operator_name { get; set; }
+
+            public RuleResponse(Rule rule)
+            {
+                this.uid = rule.uid;
+                this.code = rule.code;
+                this.organization_uid = rule.organization_uid;
+                this.owner_user_uid = rule.owner_user_uid;
+                this.group_uid = rule.group_uid;
+                this.name = rule.name;
+                this.metric_data_type_code = rule.metric_data_type_code;
+                this.operator_code = rule.operator_code;
+                this.rule_value = rule.rule_value;
+                this.create_date = rule.create_date;
+
+                if (this.operator_code == "==") this.operator_name = "=";
+                else if (this.operator_code == "!=") this.operator_name = "≠";
+                else if (this.operator_code == ">=") this.operator_name = "≥";
+                else if (this.operator_code == ">") this.operator_name = ">";
+                else if (this.operator_code == "<=") this.operator_name = "≤";
+                else if (this.operator_code == ">") this.operator_name = "<";
+            }
+        }
     }
 }
